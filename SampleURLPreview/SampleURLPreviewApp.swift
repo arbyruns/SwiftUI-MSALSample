32c201927a4fce3e7f6690bd0e6425e2a0bb7da6
Rob Evans <robevans@microsoft.com>
2022-01-10 21:11:57 -0500

    initial commit

---
+++ SampleURLPreview/SampleURLPreviewApp.swift
@@ -0,0 +1,17 @@
+//
+//  SampleURLPreviewApp.swift
+//  SampleURLPreview
+//
+//  Created by robevans on 1/6/22.
+//
+
+import SwiftUI
+
+@main
+struct SampleURLPreviewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
